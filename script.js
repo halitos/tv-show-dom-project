17fcc7e2d3eaa56a29a502d9f0b475f394ab42cc
Halit <60817499+halitos@users.noreply.github.com>
2020-06-21 00:41:48 +0100

    started trying with single div - single episode

--- script.js
+++ script.js
@@ -4,9 +4,44 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
+const main = document.getElementById("root");
+const episodeCard = document.querySelector(".card");
+const episodeTitle = document.getElementById("title");
+const episodeImg = document.getElementById("image");
+const episodeSum = document.getElementById("summary");
+const seasonNum = document.getElementById("seriNum");
+const episodeNum = document.getElementById("epNum");
+
+// function makePageForEpisodes(episodeList) {
+//   episodeTitle.textContent = `${episodeList[0].name}`;
+//   seasonNum.textContent = `${episodeList[0].season}`;
+//   episodeNum.textContent = `${episodeList[0].number}`;
+//   episodeImg.setAttribute("src", `${episodeList[0].image.medium}`);
+//   episodeSum.textContent = `${episodeList[0].summary}`;
+// }
+
 function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  episodeList.forEach(function (episode) {
+    episodeTitle.textContent = episode.name;
+    seasonNum.textContent = episode.season;
+    episodeNum.textContent = episode.number;
+    episodeImg.setAttribute("src", episode.image.medium);
+    episodeSum.textContent = episode.summary;
+  });
 }
+
+// function createNewList(episodeList) {
+//   return episodeList
+//     .map(function (item) {
+//       return `<div class="episode">
+//     <h1 class="episodeHeader">${
+//       item.name
+//     } - S${item.season.toString().padStart(2, "0")}E${item.number.toString().padStart(2, "0")}</h1>
+//     <img src=${item.image.medium} alt= ${item.name}>
+//     ${item.summary}
+//     </div>`;
+//     })
+//     .join("");
+// }
 
 window.onload = setup;
