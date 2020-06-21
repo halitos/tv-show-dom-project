176868d7600e34b63835bb52bc9f750e94fa53fe
Halit <60817499+halitos@users.noreply.github.com>
2020-06-21 10:40:44 +0100

    managed to display all episodes

--- script.js
+++ script.js
@@ -4,39 +4,40 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
-const main = document.getElementById("root");
-
-const episodeCard = document.createElement("div");
-episodeCard.id = "card"; // will be the last thing
-
-const cardHead = document.createElement("div");
-cardHead.id = "cardHeader";
-episodeCard.appendChild(cardHead);
-
-const episodeTitle = document.createElement("h3");
-episodeTitle.id = "name";
-cardHead.appendChild(episodeTitle);
-
-const seasonNum = document.createElement("h6");
-seasonNum.id = "seriNum";
-cardHead.appendChild(seasonNum);
-
-const episodeImg = document.createElement("img");
-episodeImg.id = "image";
-episodeCard.appendChild(episodeImg);
-
-const episodeSum = document.createElement("p");
-episodeSum.id = "summary";
-episodeCard.appendChild(episodeSum);
-
 function makePageForEpisodes(episodeList) {
-  episodeList.map(function (episode) {
+  episodeList.map(function (episode, index) {
+    const main = document.getElementById("root");
+
+    const episodeCard = document.createElement("div");
+    episodeCard.className = "card";
+
+    const cardHead = document.createElement("div");
+    cardHead.id = "cardHeader";
+    episodeCard.appendChild(cardHead);
+
+    const episodeTitle = document.createElement("h3");
+    episodeTitle.id = "name";
+    cardHead.appendChild(episodeTitle);
+
+    const seasonNum = document.createElement("h6");
+    seasonNum.id = "seriNum";
+    cardHead.appendChild(seasonNum);
+
+    const episodeImg = document.createElement("img");
+    episodeImg.id = "image";
+    episodeCard.appendChild(episodeImg);
+
+    const episodeSum = document.createElement("p");
+    episodeSum.id = "summary";
+    episodeCard.appendChild(episodeSum);
+
     episodeTitle.textContent = episode.name;
     seasonNum.textContent = `S${episode.season
       .toString()
       .padStart(2, "0")}  E${episode.number.toString().padStart(2, "0")}`;
     episodeImg.setAttribute("src", episode.image.medium);
     episodeSum.textContent = episode.summary;
+    episodeCard.id = "card" + index;
     main.appendChild(episodeCard);
   });
 }
