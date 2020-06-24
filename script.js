8494b4bae664c8169db32d95c5d73577b5928586
Halit <60817499+halitos@users.noreply.github.com>
2020-06-24 11:14:13 +0100

    cleaned p tags/filtered ep numbers not showing

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function makePageForEpisodes(episodeList) {
 
   main.innerHTML = `<div id="search-episodes">
   <span class="search-bar">Search episodes</span>
-  <input type="search" class="search-episodes" placeholder="Search ...">
+  <input type="search" class="search-episodes" placeholder="Search ..."><span id ="numOfDisplay"></span>
   </div>`;
 
   // -----------------populate cards-------------
@@ -48,7 +48,7 @@ function makePageForEpisodes(episodeList) {
       .toString()
       .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
     episodeImg.setAttribute("src", episode.image.medium);
-    episodeSum.textContent = episode.summary;
+    episodeSum.textContent = episode.summary.replace(/<[^>]*>/g, " ");
     episodeCard.id = "card" + index;
     container.appendChild(episodeCard);
     main.appendChild(container);
@@ -59,6 +59,8 @@ function makePageForEpisodes(episodeList) {
 
     searchBox.addEventListener("input", function () {
       const searchBoxValue = searchBox.value.toLowerCase();
+      const displayNum = document.getElementById("numOfDisplay");
+      const cardList = document.getElementsByClassName("card");
       if (
         episode.summary.toLowerCase().indexOf(searchBoxValue) > -1 ||
         episode.name.toLowerCase().indexOf(searchBoxValue) > -1
@@ -67,8 +69,25 @@ function makePageForEpisodes(episodeList) {
       } else {
         episodeCard.style.display = "none";
       }
+      displayNum.innerText = ` Displaying ${cardList.length}/${episodeList.length} episodes`;
     });
   });
 }
+
+// function displayFilteredEpisodes() {
+//   const cardList = document.getElementsByClassName("card");
+//   const filteredList = [];
+//   const filteredList = cardList.map((card) => {
+//     if (
+//       card.summary.toLowerCase().indexOf(searchBoxValue) > -1 ||
+//       card.name.toLowerCase().indexOf(searchBoxValue) > -1
+//     ) {
+//       episodeCard.style.display = "";
+//     } else {
+//       episodeCard.style.display = "none";
+//     }
+//     displayNum.innerText = ` Displaying ${cardList.length}/${episodeList.length} episodes`;
+//   });
+// }
 
 window.onload = setup;
