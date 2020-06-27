d5ef233dddf76cb07bb637d1fdbf0227249d7193
Halit <60817499+halitos@users.noreply.github.com>
2020-06-27 21:33:32 +0100

    applied some recommendation-variables moved out and functions separated

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
-//-----------------setup function-------------------
+//---------------globals-----------------
+
+const allEpisodes = getAllEpisodes();
+const main = document.getElementById("root");
+
+//-------------setup function------------
+
 function setup() {
-  const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
+//---------load page with episode cards---------
+
 function makePageForEpisodes(episodeList) {
-  const main = document.getElementById("root");
-
-  //-----------create HTML for search menu-----------
-
-  main.innerHTML = `<div id="search-episodes">
-  <select id="episode-selector"><option value="none">See all episodes</option></select>
-  <input type="search" class="search-episodes" placeholder="Search episodes...">
-  <span id ="numOfDisplay"></span>
-  </div>`;
-
-  // -----------------populate cards-------------
-
   const container = document.createElement("div");
   container.id = "cardContainer";
-  let episodeNum;
-  let episodeCard;
 
   episodeList.forEach(function (episode, index) {
-    episodeCard = document.createElement("div");
+    const episodeCard = document.createElement("div");
     episodeCard.className = "card";
 
     const cardHead = document.createElement("div");
@@ -34,7 +27,7 @@ function makePageForEpisodes(episodeList) {
     episodeTitle.id = "name";
     cardHead.appendChild(episodeTitle);
 
-    episodeNum = document.createElement("h4");
+    const episodeNum = document.createElement("h4");
     episodeNum.id = "seriNum";
     cardHead.appendChild(episodeNum);
 
@@ -56,52 +49,53 @@ function makePageForEpisodes(episodeList) {
     container.appendChild(episodeCard);
     main.appendChild(container);
   });
+}
 
-  //---------------Search event---------------
+//---------------Search event---------------
 
-  const searchBox = document.querySelector(".search-episodes");
+const searchBox = document.querySelector(".search-episodes");
 
-  searchBox.addEventListener("input", function () {
-    const searchBoxValue = searchBox.value.toLowerCase();
-    const displayNum = document.getElementById("numOfDisplay");
-    const cardList = document.querySelectorAll(".card");
-    let newList = Array.from(cardList);
-    newList.forEach(function (card) {
-      if (card.innerText.toLowerCase().indexOf(searchBoxValue) > -1) {
-        card.style.display = "";
-      } else {
-        card.style.display = "none";
-      }
-    });
-    let filteredList = newList.filter((item) => item.style.display === "");
-    displayNum.innerText = ` Displaying ${filteredList.length}/${episodeList.length} episodes`;
-  });
-
-  //---------------Select Menu------------------
-
-  const selector = document.getElementById("episode-selector");
-
-  episodeList.forEach((episode) => {
-    const option = `<option>S${episode.season
-      .toString()
-      .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")} - ${
-      episode.name
-    } </option>`;
-    selector.innerHTML += option;
-  });
-
-  selector.addEventListener("change", function () {
-    if (selector.value !== "none") {
-      const selectedEpisodes = episodeList.filter((episode) => {
-        const result = `S0${episode.season}E0${episode.number} - ${episode.name}`;
-        return result === selector.value;
-      });
-      makePageForEpisodes(selectedEpisodes);
+searchBox.addEventListener("input", function () {
+  const searchBoxValue = searchBox.value.toLowerCase();
+  const displayNum = document.getElementById("numOfDisplay");
+  const cardList = document.querySelectorAll(".card");
+  let newList = Array.from(cardList);
+  newList.forEach(function (card) {
+    if (card.innerText.toLowerCase().indexOf(searchBoxValue) > -1) {
+      card.style.display = "";
     } else {
-      makePageForEpisodes(episodeList);
+      card.style.display = "none";
     }
   });
-}
+  let filteredList = newList.filter((item) => item.style.display === "");
+  displayNum.innerText = ` Displaying ${filteredList.length}/${allEpisodes.length} episodes`;
+});
+
+//---------------Select Menu------------------
+
+const selector = document.getElementById("episode-selector");
+
+allEpisodes.forEach((episode) => {
+  const option = `<option>S${episode.season
+    .toString()
+    .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")} - ${
+    episode.name
+  } </option>`;
+  selector.innerHTML += option;
+});
+
+selector.addEventListener("change", function () {
+  if (selector.value !== "none") {
+    const selectedEpisodes = allEpisodes.filter((episode) => {
+      const result = `S0${episode.season}E0${episode.number} - ${episode.name}`;
+      return result === selector.value;
+    });
+    makePageForEpisodes(selectedEpisodes);
+    console.log(selectedEpisodes);
+  } else {
+    makePageForEpisodes(allEpisodes);
+  }
+});
 
 window.onload = setup;
 
@@ -116,4 +110,20 @@ window.onload = setup;
 //       episode.style.display = "none";
 //     }
 //   });
+// });
+
+// Ahmad's suggestion ****************
+
+// searchBox.addEventListener("input", function () {
+//   const searchBoxValue = searchBox.value.toLowerCase();
+//   const displayNum = document.getElementById("numOfDisplay");
+//   const cardList = document.querySelectorAll(".card");
+//   let newList = Array.from(cardList);
+
+//   const filteredList = newList.filter(function (card) {
+//     return card.innerText.toLowerCase().indexOf(searchBoxValue) > -1;
+//   });
+
+//   makePageForEpisodes(filteredList);
+//   displayNum.innerText = ` Displaying ${filteredList.length}/${allEpisodes.length} episodes`;
 // });
