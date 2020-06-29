d1a6ef5f3e9ae85ce5ea1740b4b9f7e4608864c1
Halit <60817499+halitos@users.noreply.github.com>
2020-06-29 21:22:39 +0100

    refactored after call

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-//-------------setup function------------
+//---------------global variables-----------------
+
+const main = document.getElementById("root");
+const selector = document.getElementById("episode-selector");
+const searchBox = document.querySelector(".search-episodes");
+const displayNum = document.getElementById("numOfDisplay");
+const url1 = "https://api.tvmaze.com/shows/82/episodes";
+const url2 = "https://api.tvmaze.com/shows/527/episodes";
+
+const container = document.createElement("div");
+container.id = "cardContainer";
+let allEpisodes;
+
+//-------------setup function with fetch------------
 
 function setup() {
-  fetch(url1)
+  fetch(url2)
     .then((response) => response.json())
 
     .then((response) => (allEpisodes = response))
@@ -14,23 +27,12 @@ function setup() {
     .catch((error) => console.log(error));
 }
 
-//---------------global variables-----------------
-
-const main = document.getElementById("root");
-const selector = document.getElementById("episode-selector");
-const searchBox = document.querySelector(".search-episodes");
-const url1 = "https://api.tvmaze.com/shows/82/episodes";
-const url2 = "https://api.tvmaze.com/shows/527/episodes";
-let container;
-let displayNum;
-let allEpisodes;
-
-//---------load page with episode cards---------
+//---------populate episode cards---------
 
 function makePageForEpisodes(episodeList) {
-  container = document.createElement("div");
-  container.id = "cardContainer";
+  container.innerHTML = "";
   makeSelector();
+  displayNum.innerText = ` Displaying ${episodeList.length}/${allEpisodes.length} episodes`;
 
   episodeList.forEach(function (episode, index) {
     const episodeCard = document.createElement("div");
@@ -56,10 +58,7 @@ function makePageForEpisodes(episodeList) {
     episodeSum.id = "summary";
     episodeCard.appendChild(episodeSum);
 
-    episodeNum.textContent = `S${episode.season
-      .toString()
-      .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`;
-
+    episodeNum.textContent = formatEpisodeNum(episode.season, episode.number);
     episodeTitle.textContent = episode.name;
     episodeImg.setAttribute("src", episode.image.medium);
     episodeSum.textContent = episode.summary.replace(/<[^>]*>/g, " ");
@@ -69,11 +68,20 @@ function makePageForEpisodes(episodeList) {
   });
 }
 
+//-----------Episode Num Formatter--------------
+
+formatEpisodeNum = function (season, episode) {
+  return `S${season.toString().padStart(2, "0")}E${episode
+    .toString()
+    .padStart(2, "0")}`;
+};
+
 //---------------Search event---------------
 
-searchBox.addEventListener("input", function () {
-  const searchBoxValue = searchBox.value.toLowerCase();
-  displayNum = document.getElementById("numOfDisplay");
+searchBox.addEventListener("input", searchForInput);
+
+function searchForInput(event) {
+  const searchBoxValue = event.target.value.toLowerCase();
   const cardList = document.querySelectorAll(".card");
   let newList = Array.from(cardList);
   newList.forEach(function (card) {
@@ -85,7 +93,7 @@ searchBox.addEventListener("input", function () {
   });
   let filteredList = newList.filter((item) => item.style.display === "");
   displayNum.innerText = ` Displaying ${filteredList.length}/${allEpisodes.length} episodes`;
-});
+}
 
 //---------Create Select Options--------------
 
@@ -102,11 +110,12 @@ function makeSelector() {
 
 //-------------select event------------------
 
-selector.addEventListener("change", function () {
-  if (selector.value === "none") {
+selector.addEventListener("change", selectFromMenu);
+
+function selectFromMenu(event) {
+  if (event.target.value === "none") {
     container.innerHTML = "";
     makePageForEpisodes(allEpisodes);
-    displayNum.innerText = "";
   } else {
     const selectedEpisode = allEpisodes.filter((episode) => {
       return (
@@ -121,33 +130,6 @@ selector.addEventListener("change", function () {
     makePageForEpisodes(selectedEpisode);
   }
   selector.value = "";
-});
+}
 
 window.onload = setup;
-
-// ***********Trying Ahmad's suggestion ************
-
-// searchBox.addEventListener("input", function () {
-//   const searchBoxValue = searchBox.value.toLowerCase();
-//   const displayNum = document.getElementById("numOfDisplay");
-//   const cardList = document.querySelectorAll(".card");
-//   let newList = Array.from(cardList);
-
-//   const filteredList = newList.filter(function (card) {
-//     return card.innerText.toLowerCase().indexOf(searchBoxValue) > -1;
-//   });
-
-//   makePageForEpisodes(filteredList);
-//   displayNum.innerText = ` Displaying ${filteredList.length}/${allEpisodes.length} episodes`;
-// });
-
-// searchBox.addEventListener("input", function () {
-//   const searchBoxValue = searchBox.value.toLowerCase();
-//   displayNum = document.getElementById("numOfDisplay");
-
-//   let filteredList = allEpisodes.filter(function (episode) {
-//     episode.innerText.toLowerCase().indexOf(searchBoxValue) > -1;
-//   });
-//   makePageForEpisodes(filteredList);
-//   displayNum.innerText = ` Displaying ${filteredList.length}/${allEpisodes.length} episodes`;
-// });
